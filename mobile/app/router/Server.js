6e8513f495ea90ace642a07eb10d8e7a431480c0
ndatn <ndaten0@gmail.com>
2023-11-11 03:33:31 +0700

    add

---
+++ mobile/app/router/Server.js
@@ -0,0 +1,29 @@
+// const express = require('express');
+// const app = express();
+// const port = 5000;
+
+// const comics = [
+//   {
+//     title: 'Naruto',
+//     description: 'This is naruto comic',
+//     imageUrl:
+//       'http://res.cloudinary.com/justme2002cloud/image/upload/v1699623468/HD-wallpaper-naruto-anime-manga-naruto-uzumaki_rmi7er.jpg',
+//     authorId: 'dcab37a0-7d38-41f2-5aba-08dbe1f047a9',
+//     author: null,
+//     categoryId: '0d5dedd9-bfd7-48cf-65e5-08dbe1f00be5',
+//     category: null,
+//     comicEpisodes: [],
+//     id: 'de9717e0-f8ba-4be1-6062-08dbe1f23a9f',
+//     modifiedAt: '2023-11-10T20:37:48.4391068',
+//     isDeleted: false,
+//   },
+// ];
+
+// app.get('..Api/Comic', (req, res) => {
+//   res.json(comics);
+// });
+
+// // Bắt đầu server
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });

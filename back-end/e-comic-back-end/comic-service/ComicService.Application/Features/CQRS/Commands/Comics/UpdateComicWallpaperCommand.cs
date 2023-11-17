b352cc321b51cf47856b8b177921ec60ab67dd86
huy.hoang <huy.hoang@orientsoftware.com>
2023-11-17 13:51:47 +0700

    update: wallpaper for comic

---
+++ back-end/e-comic-back-end/comic-service/ComicService.Application/Features/CQRS/Commands/Comics/UpdateComicWallpaperCommand.cs
@@ -0,0 +1,42 @@
+﻿using ComicService.Application.Services.CloudinaryService.Interfaces;
+using ComicService.Infrastructure.CQRS.Command;
+using ComicService.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicService.Application.Features.CQRS.Commands.Comics
+{
+    public class UpdateComicWallpaperCommand : ICommand<CommandResult<Guid>>
+    {
+        public Guid Id { get; set; }
+        public string FilePath { get; set; }
+    }
+
+    public class UpdateComicWallpaperCommandHandler : ICommandHandler<UpdateComicBackgroundImageCommand, CommandResult<Guid>>
+    {
+        private readonly IComicRepository _repository;
+        private readonly ICloudinaryService _cloudinaryService;
+
+        public UpdateComicWallpaperCommandHandler(IComicRepository repository, ICloudinaryService cloudinaryService)
+        {
+            _repository = repository;
+            _cloudinaryService = cloudinaryService;
+        }
+
+        public async Task<CommandResult<Guid>> Handle(UpdateComicBackgroundImageCommand request, CancellationToken cancellationToken)
+        {
+            var comic = _repository.FirstOrDefault(_ => _.Id == request.Id);
+            if (comic is null)
+                return CommandResult<Guid>.Error("Comic is not found");
+            var url = await _cloudinaryService.UploadImageAsync(request.Path);
+            if (url is null)
+                return CommandResult<Guid>.Error("Failed to upload image to cloud service");
+            comic.UpdateWallPaperUrl(url);
+            _repository.Update(comic);
+            return CommandResult<Guid>.Success(comic.Id);
+        }
+    }
+}
---
+++ back-end/e-comic-back-end/comic-service/ComicService.Persistence/Migrations/20231117034051_add-wallpaper-column.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComicService.Persistence.Migrations
+{
+    public partial class addwallpapercolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WallPaperUrl",
+                table: "Comics",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WallPaperUrl",
+                table: "Comics");
+        }
+    }
+}
